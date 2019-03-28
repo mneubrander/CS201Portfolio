efb424cc58890a6d3736d5b30359f61386d958ed
mneubrander <mneubrander@crimson.ua.edu>
2019-03-28 15:46:08 -0500

    Updated script to get dictionary file

--- PrepareScript.sh
+++ PrepareScript.sh
@@ -4,3 +4,13 @@ cd American
 tr -d '%' < 2of12inf.txt > temp.txt && mv temp.txt 2of12inf.txt
 tr -d '!' < 2of12inf.txt > temp.txt && mv temp.txt 2of12inf.txt
 mv 2of12inf.txt BoggleDictionary.txt
+cd ..
+cp American/BoggleDictionary.txt BoggleDictionary.txt
+rm -rf American
+rm -rf International
+rm -rf Lemmatized
+rm -rf Special
+rm agid.txt
+rm alllists.html
+rm 'download?use_mirror=svwh'
+rm ReadMe.html

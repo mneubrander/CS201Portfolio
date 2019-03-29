0a0b8a409384bd75b6bf96e72df6e5a935af7aa0
mneubrander <mneubrander@crimson.ua.edu>
2019-03-29 03:16:49 +0100

    Added missing quotation mark

--- gameplay.c
+++ gameplay.c
@@ -702,7 +702,7 @@ void DisplayInstructions(struct TrieNode * dictionary) {
         Though initially this may seem like playing against the computer, it is quite different\n\
         - the only person you can lose to (or beat!) is yourself. Here, the computer keeps track\n\
         of the high score of your consecutive matches. You may not change board size between matches,\n\
-        as comparing raw scores between different size boards doesn't make sense. \n
+        as comparing raw scores between different size boards doesn't make sense. \n\
   \n\
       ONE PLAYER - SAVE PROGRESS:\n\
         This is just like one player, but saves your high score between sessions. Why would you use one player\n\
